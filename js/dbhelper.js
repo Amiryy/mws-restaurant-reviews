f79b967322d6c3780d9878faa5210ebec9cc7b7f
Amir Yuval <amiryu@qwilt.com>
2018-06-01 02:13:02 +0300

    fetching data from server with fetch

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,26 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 3000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants/`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+    const url = DBHelper.DATABASE_URL;
+    fetch(url).then(response => {
+      if(response.status === 200) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
       }
-    };
-    xhr.send();
+    }).then(data => {
+      callback(null, data);
+    }).catch(error => {
+      callback(error, null);
+    });
   }
 
   /**
@@ -150,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}.jpg`);
   }
 
   /**
